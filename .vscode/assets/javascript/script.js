8a6d7c095ed3b18d590867f9d69f23dbf5a17957
Stephen <sjohnston.ie@gmail.com>
2021-04-20 21:58:54 +0000

    feat: Add event listener keydown for enter for submit button

--- .vscode/assets/javascript/script.js
+++ .vscode/assets/javascript/script.js
@@ -15,6 +15,12 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
+    document.getElementById('answer-box').addEventListener('keydown', function(Event){
+        if (event.key === 'Enter'){
+            checkAnswer();
+        }
+    })
+
     runGame('addition');
 })
 
@@ -26,6 +32,9 @@ function runGame(gameType){
     // Generate two random numbers between 1 and 25
     // Math.floor rounds the number down to a whole number
     // Math.random generates random numbers between 0 and 1
+
+    document.getElementById('answer-box').value = '';
+    document.getElementById('answer-box').focus();
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
